c3f4f7c7b9f57867aa6196567e462e971cf66e79
snigdhalaxetti-cmd <snigdhalaxetti@gmail.com>
2026-05-28 11:34:49 +0530

    Add files via upload

---
+++ task1/script.js
@@ -0,0 +1,11 @@
+// Selecting button
+
+const welcomeBtn = document.getElementById("welcomeBtn");
+
+// Adding click event
+
+welcomeBtn.addEventListener("click", function(){
+
+    alert("Welcome to my Internship Task 1 Project!");
+
+});
